3df418679992879249e6a97216d157f7447656c3
lynne003 <1010673763@qq.com>
2023-07-15 12:17:47 +0800

    添加controller,和service

--- achieveWeb/trunk/src/main/java/com/le/achieveweb/controller/UserController.java
+++ achieveWeb/trunk/src/main/java/com/le/achieveweb/controller/UserController.java
@@ -1,4 +1,29 @@
 package com.le.achieveweb.controller;
 
+import com.le.achieveweb.entity.UserLogin;
+import com.le.achieveweb.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/userLogin")
 public class UserController {
+
+    @Autowired
+    UserService userService;
+
+    // 登录
+    @PostMapping("/login")
+    public String login(@RequestBody UserLogin user) {
+        return userService.login(user);
+    }
+
+    // 注册
+    @PostMapping("/register")
+    public String register(@RequestBody UserLogin user) {
+        return userService.register(user);
+    }
 }
---
+++ achieveWeb/trunk/src/main/java/com/le/achieveweb/service/UserService.java
@@ -0,0 +1,52 @@
+package com.le.achieveweb.service;
+
+import com.le.achieveweb.dao.UserMapper;
+import com.le.achieveweb.entity.UserLogin;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    //autowire:可以对类成员变量、方法以及构造函数进行标注，完成自动装配工作
+    UserMapper userMapper;//用接口名定义变量：这只是一个接口的引用，接口不能实例化对象，而接口的引用指向的是实现了接口方法的类的实例化对象。
+
+    // 登录操作
+    public String login(UserLogin user) {
+        try {
+            UserLogin userExistN = userMapper.queryByName(user.getUsername());
+            if (userExistN != null) {
+                String userExistP = userMapper.queryPswByName(user.getUsername());
+                if (userExistP.equals(user.getPassword())) {
+                    return "登录成功";
+                } else {
+                    return "密码错误" ;
+                }
+            } else {
+                return ".";
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return e.getMessage();
+        }
+    }
+
+
+    // 注册操作
+    public String register(UserLogin user) {
+        try {
+            UserLogin userExist = userMapper.queryByName(user.getUsername());
+            if (user.getUsername().equals("")) {
+                return "用户名不能为空";
+            } else if (user.getPassword().equals("")) {
+                return "密码不能为空";
+            } else if (userExist != null) {
+                return "用户已经存在";
+            } else {
+                userMapper.save(user);
+                return "注册成功";
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return e.getMessage();
+        }
+    }}
