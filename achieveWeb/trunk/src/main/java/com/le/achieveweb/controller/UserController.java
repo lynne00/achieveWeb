a08a3760e526b71bc7e37c1e5bb6e5b53669aefa
lynne003 <1010673763@qq.com>
2023-07-17 14:22:10 +0800

    创建bean时出错

--- achieveWeb/trunk/src/main/java/com/le/achieveweb/controller/UserController.java
+++ achieveWeb/trunk/src/main/java/com/le/achieveweb/controller/UserController.java
@@ -3,6 +3,7 @@ package com.le.achieveweb.controller;
 import com.le.achieveweb.entity.UserLogin;
 import com.le.achieveweb.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,14 +12,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/userLogin")
 public class UserController {
-
     @Autowired
     UserService userService;
-
     // 登录
     @PostMapping("/login")
     public String login(@RequestBody UserLogin user) {
-        return userService.login(user);
+//        @RequestBody UserLogin user
+         return userService.login(user);
+//        return "hello";
     }
 
     // 注册
--- achieveWeb/trunk/src/main/java/com/le/achieveweb/service/UserService.java
+++ achieveWeb/trunk/src/main/java/com/le/achieveweb/service/UserService.java
@@ -2,11 +2,12 @@ package com.le.achieveweb.service;
 
 import com.le.achieveweb.dao.UserMapper;
 import com.le.achieveweb.entity.UserLogin;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
-
+    @Autowired
     //autowire:可以对类成员变量、方法以及构造函数进行标注，完成自动装配工作
     UserMapper userMapper;//用接口名定义变量：这只是一个接口的引用，接口不能实例化对象，而接口的引用指向的是实现了接口方法的类的实例化对象。
 
