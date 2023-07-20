6e425f1c1ba4672cf33eddff15c37a3d537c221b
lynne003 <1010673763@qq.com>
2023-07-20 17:36:11 +0800

    优化password单元测试，随机，大批量测试

--- achieveWeb/trunk/src/test/java/com/le/achieveweb/util/PasswordUtilTest.java
+++ achieveWeb/trunk/src/test/java/com/le/achieveweb/util/PasswordUtilTest.java
@@ -1,26 +1,30 @@
 package com.le.achieveweb.util;
-import org.junit.jupiter.api.Test;
+import cn.hutool.core.util.RandomUtil;
+import org.junit.jupiter.api.RepeatedTest;
 import static org.junit.jupiter.api.Assertions.*;
 public class PasswordUtilTest {
-    @Test
+    @RepeatedTest(500)
     public void testHashPassword() {
-        String password = "testpassword";
-        String hashedPassword = PasswordUtil.hashPassword(password);
+        int length = RandomUtil.randomInt(9, 25); // 生成随机长度，范围在9到25之间
+        String randomPassword = RandomUtil.randomString(length);// 生成随机密码
+        String hashedPassword = PasswordUtil.hashPassword(randomPassword);
         // 确保加密后的密码不等于原始密码
-        assertFalse(password.equals(hashedPassword));
+        assertFalse(randomPassword.equals(hashedPassword));
     }
-    @Test
+    @RepeatedTest(500)
     public void testCheckPassword() {
-        String password = "testpassword";
-        String hashedPassword = PasswordUtil.hashPassword(password);
+        int length = RandomUtil.randomInt(9, 25); // 生成随机长度，范围在9到25之间
+        String randomPassword = RandomUtil.randomString(length);// 生成随机密码
+        String hashedPassword = PasswordUtil.hashPassword(randomPassword);
         // 验证密码正确性
-        assertTrue(PasswordUtil.checkPassword(password, hashedPassword));
+        assertTrue(PasswordUtil.checkPassword(randomPassword, hashedPassword));
     }
-    @Test
+    @RepeatedTest(500)
     public void testCheckPasswordIncorrect() {
-        String password = "testpassword";
-        String wrongPassword = "wrongpassword";
-        String hashedPassword = PasswordUtil.hashPassword(password);
+        int length = RandomUtil.randomInt(9, 25); // 生成随机长度，范围在9到25之间
+        String randomPassword = RandomUtil.randomString(length);// 生成随机密码
+        String wrongPassword =  RandomUtil.randomString(length);// 生成随机错误密码
+        String hashedPassword = PasswordUtil.hashPassword(randomPassword);
         // 验证错误的密码
         assertFalse(PasswordUtil.checkPassword(wrongPassword, hashedPassword));
     }
